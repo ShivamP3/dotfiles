bbd89756a37bbe3cc782b8f6970821fb3acff3e5
ShivamP3 <shivam.01010000@gmail.com>
2022-08-23 17:04:45 -0400

    fix: fish update-application-list function

--- .config/fish/functions/update-application-list.fish
+++ .config/fish/functions/update-application-list.fish
@@ -1,6 +1,6 @@
 function update-application-list
 	pacman -Qqen > $DOTFILES/pacman-list
 	pacman -Qqm > $DOTFILES/aur-list
-	cargo install --list | awk '/^\w/ { print $1 }' | awk NF=1 > /home/rejyr/dotfiles/cargo-list
-	pip freeze > /home/rejyr/dotfiles/pip-list
+	cargo install --list | awk '/^\w/ { print $1 }' | awk NF=1 > /home/shivam/dotfiles/cargo-list
+	pip freeze > /home/shivam/pip-list
 end
